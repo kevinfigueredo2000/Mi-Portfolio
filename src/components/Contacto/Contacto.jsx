9c41f00e380f14f26af5cf397f0a2a8aca2f42e2
kevinfigueredo <figueredo.kevin2000@gmail.com>
2022-04-14 01:15:15 -0300

    dsadas

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -3,11 +3,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./Contacto.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
-import emailjs from '@emailjs/browser';
-import { useRef } from "react";
+import emailjs from "emailjs-com"
 
 export const Contacto = () =>{
-    const form = useRef();
     function sendEmail(e){
         e.preventDefault();
     
@@ -32,7 +30,7 @@ export const Contacto = () =>{
                         <p>Cualquier consulta o duda deje su mensaje en el formulario de contacto o bien un Whatsapp mediante el botón de la esquina inferior derecha.</p>
                     </div>
                     <div className="col-12 col-sm-6">
-                        <form ref={form} onSubmit={sendEmail}>
+                        <form onSubmit={sendEmail}>
                             <input type="text" name="user_name" id="nombre" placeholder="Nombre" className="shadow form-control"/><br/>
                             <input type="email" name="user_email" id="email" placeholder="Email" className="shadow form-control  mt-1 mb-3"/>
                             <textarea name="message" id="comentarios" cols="78" rows="10" placeholder="Comentarios..." className=" shadow form-control"/>
