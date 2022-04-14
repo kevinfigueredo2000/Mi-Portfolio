7261d2f1f554f82854fbe399f6ec692f3fe4913b
kevinfigueredo <figueredo.kevin2000@gmail.com>
2022-04-14 17:27:14 -0300

    cambios

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
 
 
 export const NavBar = () =>{
-    function estadoBarra(){
+    const estadoBarra = ()=>{
         document.getElementById("nav-ul").removeClass("show")
         document.getElementById("nav-ul").classList.toggle("hide");
     }
@@ -22,17 +22,17 @@ export const NavBar = () =>{
                             <FontAwesomeIcon className="fas fa-bars" icon={faBars}/>
                         </button>
                         <ul className="nav-bar nav nav-ul" id="nav-ul">
-                            <li className="nav-item" onClick={()=>{estadoBarra()}}>
-                                <Link to="/" className="nav-link item" >Inicio</Link>
+                            <li className="nav-item ">
+                                <Link to="/" className="nav-link item" onClick={estadoBarra}>Inicio</Link>
                             </li>
-                            <li className="nav-item" onClick={()=>{estadoBarra()}}>
-                                <Link to="sobre-mi" className="nav-link item" >Sobre mi</Link>
+                            <li className="nav-item ">
+                                <Link to="sobre-mi" className="nav-link item" onClick={estadoBarra}>Sobre mi</Link>
                             </li>
-                            <li className="nav-item" onClick={()=>{estadoBarra()}}>
-                                <Link to="proyectos" className="nav-link item">Proyectos</Link>
+                            <li className="nav-item ">
+                                <Link to="proyectos" className="nav-link item" onClick={estadoBarra}>Proyectos</Link>
                             </li>
-                            <li className="nav-item" onClick={()=>{estadoBarra()}}>
-                                <Link to="contacto" className="nav-link item">Contacto</Link>
+                            <li className="nav-item ">
+                                <Link to="contacto" className="nav-link item" onClick={estadoBarra}>Contacto</Link>
                             </li>
                         </ul>
                         <div id="cvv">
