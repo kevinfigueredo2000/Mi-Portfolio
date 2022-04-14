9aedfe68838c4f1abd0c4484f9b4fa53c42b8c22
kevinfigueredo <figueredo.kevin2000@gmail.com>
2022-04-14 18:17:29 -0300

    whatsapp mensaje

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -22,7 +22,7 @@ export const Footer = () =>{
                     </div>
                 </Row>      
                 <Row>
-                    <a href="https://web.whatsapp.com/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faWhatsapp} className="fa-whatsapp" id="fa-whatsapp"/></a>
+                    <a href="https://api.whatsapp.com/send?phone=+541151212855&text=%C2%A1Hola!%20Gracias%20por%20contactar%20conmigo.%20Deja%20tu%20mensaje,%20te%20respondere%20en%20breve." target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faWhatsapp} className="fa-whatsapp" id="fa-whatsapp"/></a>
                 </Row>
             </Container> 
         </>
