ba6a1e6a87533348a45f496c3ec492042799e2f5
kevinfigueredo <figueredo.kevin2000@gmail.com>
2022-04-28 01:20:55 -0300

    cambios

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,10 +10,10 @@ export const NavBar = () =>{
     window.onscroll = function(){
         let Desplazamiento_Actual = window.pageYOffset;
         if(ubicacionPrincipal >= Desplazamiento_Actual){
-            document.getElementById('nav').classList = 'smooth-scroll';
+            document.getElementById('nav').classList = 'smooth-scroll bg-black';
             document.getElementById('nav').style.top = '0';
         }else{
-            document.getElementById('nav').classList = 'smooth-scroll';
+            document.getElementById('nav').classList = 'smooth-scroll bg-black';
             document.getElementById('nav').style.top = '-100px';
         }
         ubicacionPrincipal = Desplazamiento_Actual;
