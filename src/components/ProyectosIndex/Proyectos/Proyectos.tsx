caa5040b02ed7b52fe3e8cbea7c1b08498d875e8
Kevin Figueredo <74889406+kevinfigueredo2000@users.noreply.github.com>
2023-03-07 21:57:47 -0300

    full responsive

--- src/components/ProyectosIndex/Proyectos/Proyectos.tsx
+++ src/components/ProyectosIndex/Proyectos/Proyectos.tsx
@@ -6,8 +6,8 @@ import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai"
 import { useState } from "react";
 import { ProjectsJsnD } from "./ProyectosD"
 
-interface Props{
-    Projects:Array<ProjectsJsnD>
+interface Props {
+    Projects: Array<ProjectsJsnD>
 }
 
 export const Proyectos = (prop: Props) => {
@@ -16,7 +16,7 @@ export const Proyectos = (prop: Props) => {
 
     itemCarouselNumber === 0 && setItemCarouselNumber(Number(prop.Projects.length + 2));
 
-    const handleSelect = (prop:boolean) => { //
+    const handleSelect = (prop: boolean) => { //
         if (prop === false) {
             setItemCarouselNumber(itemCarouselNumber + 3);
         }
@@ -24,52 +24,92 @@ export const Proyectos = (prop: Props) => {
             setItemCarouselNumber(itemCarouselNumber - 3);
         }
     };
-    return (
-        <Row>
-            <div className="col">
-                <AiOutlineLeft onClick={() => {handleSelect(true); itemCarouselNumber <= (prop.Projects?.length) && setItemCarouselNumber(prop.Projects?.length + 1)}} id="AiOutlineLeft" />
-            </div>
-            <div className="col-10" >
-                <Row>
-                    {
-                        prop.Projects && prop.Projects.slice(itemCarouselNumber - 3, itemCarouselNumber).map(projects => {
-                            return (
-                                <div className="col-sm-4" style={{ transition: "1s all ease" }}>
-                                    <div className="card shadow mb-5">
-                                        <div className="mx-3 my-3">
-                                            <Row className="mb-3 mx-2">
-                                                <a href={projects.link} target="_blank" rel="noreferrer"><img src={projects.icon} className="imgFluida shadow" alt={projects.name} /></a>
-                                            </Row>
-                                            <Row className="sv">
-                                                <h2 className="my-2 text-center nombres-pr">{projects.name}</h2>
-                                            </Row>
-                                            <Row className="mx-1 my-3 desc">
-                                                <p>{projects.description}</p>
-                                            </Row>
-                                            <Container>
-                                                <Row className="mb-5" id="Row-VOnline-CFuente">
-                                                    <div className="col-sm-8 col">
-                                                        <a href={projects.link} target="_blank" type="button" id="btn_ver" rel="noreferrer">VER ONLINE</a>
+    if (window.screen.width <= 800) {
+        return (
+            <Row>
+                {
+                    prop.Projects && prop.Projects.map(projects => {
+                        return (
+                            <div className="col-sm-4" style={{ transition: "1s all ease" }}>
+                                <div className="card shadow mb-5">
+                                    <div className="mx-3 my-3">
+                                        <Row className="mb-3 mx-2">
+                                            <a href={projects.link} target="_blank" rel="noreferrer"><img src={projects.icon} className="imgFluida shadow" alt={projects.name} /></a>
+                                        </Row>
+                                        <Row className="sv">
+                                            <h2 className="my-2 text-center nombres-pr">{projects.name}</h2>
+                                        </Row>
+                                        <Row className="mx-1 my-3 desc">
+                                            <p>{projects.description}</p>
+                                        </Row>
+                                        <Container>
+                                            <Row className="mb-5" id="Row-VOnline-CFuente">
+                                                <div className="col-sm-8 col">
+                                                    <a href={projects.link} target="_blank" type="button" id="btn_ver" rel="noreferrer">VER ONLINE</a>
+                                                </div>
+
+                                                {
+                                                    projects.codigo &&
+                                                    <div className="col-sm-4 col">
+                                                        <a href={projects.codigo} target="_blank" className="cf" rel="noreferrer">Código fuente</a>
                                                     </div>
-
-                                                    {
-                                                        projects.codigo &&
-                                                        <div className="col-sm-4 col">
-                                                            <a href={projects.codigo} target="_blank" className="cf" rel="noreferrer">Código fuente</a>
-                                                        </div>
-                                                    }
-                                                </Row>
-                                            </Container>
-                                        </div>
+                                                }
+                                            </Row>
+                                        </Container>
                                     </div>
-                                </div>)
-                        })}
-                </Row>
-            </div>
-            <div className="col">
-                <AiOutlineRight onClick={() => { handleSelect(false); itemCarouselNumber >= (prop.Projects?.length) && setItemCarouselNumber(3) }} id="AiOutlineRight" />
-            </div>
-        </Row>
+                                </div>
+                            </div>)
+                    })}
+            </Row>
+        )
+    } else {
+        return (
+            <Row>
+                <div className="col">
+                    <AiOutlineLeft onClick={() => { handleSelect(true); itemCarouselNumber <= (prop.Projects?.length) && setItemCarouselNumber(prop.Projects?.length + 1) }} id="AiOutlineLeft" />
+                </div>
+                <div className="col-10" >
+                    <Row>
+                        {
+                            prop.Projects && prop.Projects.slice(itemCarouselNumber - 3, itemCarouselNumber).map(projects => {
+                                return (
+                                    <div className="col-sm-4" style={{ transition: "1s all ease" }}>
+                                        <div className="card shadow mb-5">
+                                            <div className="mx-3 my-3">
+                                                <Row className="mb-3 mx-2">
+                                                    <a href={projects.link} target="_blank" rel="noreferrer"><img src={projects.icon} className="imgFluida shadow" alt={projects.name} /></a>
+                                                </Row>
+                                                <Row className="sv">
+                                                    <h2 className="my-2 text-center nombres-pr">{projects.name}</h2>
+                                                </Row>
+                                                <Row className="mx-1 my-3 desc">
+                                                    <p>{projects.description}</p>
+                                                </Row>
+                                                <Container>
+                                                    <Row className="mb-5" id="Row-VOnline-CFuente">
+                                                        <div className="col-sm-8 col">
+                                                            <a href={projects.link} target="_blank" type="button" id="btn_ver" rel="noreferrer">VER ONLINE</a>
+                                                        </div>
 
-    )
+                                                        {
+                                                            projects.codigo &&
+                                                            <div className="col-sm-4 col">
+                                                                <a href={projects.codigo} target="_blank" className="cf" rel="noreferrer">Código fuente</a>
+                                                            </div>
+                                                        }
+                                                    </Row>
+                                                </Container>
+                                            </div>
+                                        </div>
+                                    </div>)
+                            })}
+                    </Row>
+                </div>
+                <div className="col">
+                    <AiOutlineRight onClick={() => { handleSelect(false); itemCarouselNumber >= (prop.Projects?.length) && setItemCarouselNumber(3) }} id="AiOutlineRight" />
+                </div>
+            </Row>
+
+        )
+    }
 }
